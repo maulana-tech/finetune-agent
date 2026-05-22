a75508203f8470431c4ac7d50b10393326a8b260
maulana-tech <firdaussyah03@gmail.com>
2026-05-22 20:09:26 +0700

    chore(apps/web/next.config.mjs): auto update

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['pg', 'drizzle-orm'],
-  experimental: {
-    fileWatcher: {
-      watchOptions: {
-        ignored: [
-          '**/.git/**',
-          '**/node_modules/**',
-          '**/.next/**',
-          '**/.pnpm/**',
-          '**/dist/**',
-          '**/.cache/**',
-        ],
-      },
-    },
-  },
 };
 
 export default nextConfig;
