cc11db4b81c2fa36572d732d1c3cc108386aff55
maulana-tech <firdaussyah03@gmail.com>
2026-05-24 14:08:10 +0700

    chore(apps/web/next.config.mjs): auto update

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['pg', 'drizzle-orm'],
+  async rewrites() {
+    return [
+      {
+        source: '/api-proxy/:path*',
+        destination: 'http://43.129.54.139:3001/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
